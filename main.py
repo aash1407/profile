bd91b6de7daa31a34268483ce4b105c9b89df4bc
aash1407 <aashritha1407@gmail.com>
2024-07-26 14:43:58 +0200

    Feat: Add description, image and links

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
+#
+# with open('wave.css') as f:
+#     css = f.read()
+
+#st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
 with col1:
-    st.image("images/photo.png", width = 400)
+    st.image("images/photo.png", width=500)
 
 with col2:
     st.title("Aashritha L S")
@@ -24,16 +30,23 @@ Feel free to contact me! :)
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"],width = 300)
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"], width = 300)
+        st.write(f"[Source Code]({row['url']})")
+
 
 
 
