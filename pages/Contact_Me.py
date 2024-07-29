5c2a473b9adbbd65a58d2051f8bd86ca8a8c8acb
aash1407 <aashritha1407@gmail.com>
2024-07-29 13:53:36 +0200

    Feat: Adding subject dropdown selectbox in contact form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -4,10 +4,11 @@ st.header("Contact Me")
 
 with st.form(key='email_form'):
     user_email = st.text_input("Your email address", key="user_email")
+    subject = st.selectbox("Select the subject",("General questions","Connect request","Interview/job offer","Collaboration","Other"))
     message = st.text_area("Your message")
     message = message
     submit = st.form_submit_button("Submit")
     if submit:
-        send_email(message, user_email)
+        send_email(message, user_email, subject)
         st.info("Your email was sent successfully!")
 
--- send_email.py
+++ send_email.py
@@ -11,18 +11,18 @@ password = "scpy qexg gaev ntjf"
 receiver = "aashritha1407@gmail.com"
 
 
-def send_email(message, user_email):
+def send_email(message, user_email, subject):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT, context=context) as server:
         server.login(username, password)
 
         # Construct email
-        subject = f"message from {user_email}"
+        subject = f"{subject}"
         msg = EmailMessage()
         msg['From'] = user_email
         msg['To'] = receiver
         msg['Subject'] = subject
-        msg.set_content(message)
+        msg.set_content(f"\nFrom: {user_email} \n" + message)
 
         server.send_message(msg)
         server.quit()
